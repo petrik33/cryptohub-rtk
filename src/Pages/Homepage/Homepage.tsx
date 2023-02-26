a2b05db8ca5802d978ef30a3b4f9403c431de047
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-02-26 11:48:39 +0300

    feat: Homepage

--- src/Pages/Homepage/Homepage.tsx
+++ src/Pages/Homepage/Homepage.tsx
@@ -1,10 +1,29 @@
+import { Col, Row, Statistic } from 'antd';
+import Title from 'antd/es/typography/Title';
 import React from 'react';
 
 const Homepage : React.FC = () => {
   return (
-    <div >
-      
-    </div>
+    <>
+      <Title level={2} className='heading'>Global Crypto Stats</Title>
+      <Row>
+        <Col span={12}>
+          <Statistic title='Total Cryptocurrencies' value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Exchanges' value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Market Cap' value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total 24h Volume' value={5} />
+        </Col>
+        <Col span={12}>
+          <Statistic title='Total Markets' value={5} />
+        </Col>
+      </Row>
+    </>
   );
 }
 
